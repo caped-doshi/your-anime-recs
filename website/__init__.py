6d7055a8a0f8068d2770aedacd63738501e542cc
Arvind Rathnashyam <greenray771@gmail.com>
2021-12-30 19:15:04 -0500

    Changes for heroku

--- website/__init__.py
+++ website/__init__.py
@@ -30,8 +30,6 @@ def create_app():
     def load_user(id):
         return User.query.get(int(id))
 
-    app = Flask(__name__, template_folder='templates') 
-
     return app
 
 def create_database(app):
